70c4ad719c64892f6b452a0219ea5ed7a9d0e843
clt556 <116059720+clt556@users.noreply.github.com>
2026-03-15 14:23:32 +0900

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Epic Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+
